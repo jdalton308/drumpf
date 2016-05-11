b069887ae07b2252e4c10459c6676af36dba6c9d
jdalton308 <jdalton308@yahoo.com>
2016-05-11 12:54:24 -0700

    Making boxes fall over

---
+++ src/js/main.js
@@ -0,0 +1,29 @@
+$(function(){
+
+	var $window = $window;
+	var $body = $('body');
+	var $main = $('main');
+	var isPortrait;
+
+	// $window.resize(function(){
+	// 	var windowHeight = window.innerHeight;
+	// 	var windowWidth = window.innerWidth;
+	// 	var isPortrait = (windowHeight > windowWidth);
+
+	// 	// $window.trigger('my_orientation_change');
+	// 	if (isPortrait) {
+	// 		$main.removeClass('landscape');
+	// 	} else {
+	// 		$main.addClass('landscape');
+	// 	}
+	// });
+
+	// $window.on('my_orientation_change', function(){
+	// 	console.log('Orientation change event');
+
+	// 	if (isPortrait) {
+
+	// 	}
+	// });
+
+});
