2eb77bc632c55a57a86be278f25241792f7670da
jdalton308 <jdalton308@yahoo.com>
2016-05-13 16:56:55 -0700

    Added images and adjusted timing

--- src/js/main.js
+++ src/js/main.js
@@ -40,6 +40,7 @@ $(function(){
 			$main.removeClass('trip');
 			isTripped = false;
 			$tripBtn.text('TRIP');
+			Quotes.next();
 		}
 
 		$tripBtn.click(function(){
@@ -52,5 +53,47 @@ $(function(){
 	}
 
 	setBtn();
+
+
+	var Quotes = (function quotes() {
+		var quotes = [
+			'If Ivanka weren’t my daughter, perhaps I’d be dating her.',
+			'My fingers are long and beautiful, as, it has been well been documented, are various other parts of my body.',
+			'I am being proven right about massive vaccinations. The doctors lied. Save our children and their future.',
+			'Laziness is a trait in the blacks. Black guys counting my money! I hate it.',
+			'The only kind of people I want counting my money are little short guys that wear yamakas every day.',
+			'26,000 unreported sexual assaults in the military. Only 238 convictions. What did these geniuses expect when they put men & women together?',
+			'If Hillary Clinton can’t satisfy her husband what makes her think she can satisfy America.',
+			'You could see there was blood coming out of her eyes. Blood coming out of her wherever.',
+			'Today I officially declare my candidacy for President of the United States.',
+			'An ‘extremely credible source’ has called my office and told me that Barack Obama’s birth certificate is a fraud.',
+			'Ariana Huffington is unattractive, both inside and out. I fully understand why her former husband left her for a man – he made a good decision.',
+			'I will build a great wall – and nobody builds walls better than me, believe me – and I’ll build them very inexpensively. I will build a great, great wall on our southern border, and I will make Mexico pay for that wall. Mark my words.',
+			'When Mexico sends its people, they’re not sending the best. They’re not sending you, they’re sending people that have lots of problems and they’re bringing those problems with us. They’re bringing drugs. They’re bring crime. They’re rapists… And some, I assume, are good people.',
+			'All of the women on The Apprentice flirted with me – consciously or unconsciously. That’s to be expected.',
+			'Our great African-American President hasn’t exactly had a positive impact on the thugs who are so happily and openly destroying Baltimore.',
+			'I have never seen a thin person drinking Diet Coke.',
+			'I think the only difference between me and the other candidates is that I’m more honest and my women are more beautiful.',
+			'The point is, you can never be too greedy.',
+			"My IQ is one of the highest — and you all know it! Please don't feel so stupid or insecure; it's not your fault.",
+			'Happy #CincoDeMayo! The best taco bowls are made in Trump Tower Grill. I love Hispanics!',
+			"We can't continue to allow China to rape our country"
+		];
+		var usedQuotes = [];
+		var $quoteBox = $('.quote-box'); 
+
+		function next() {
+			var nextIndex = Math.floor( Math.random()*quotes.length );
+			// todo: Check in index was used already
+
+			var nextQuote = quotes[nextIndex];
+			$quoteBox.text(nextQuote);
+		}
+		return {
+			next: next
+		};
+	})();
+
+	Quotes.next();
 
 });
